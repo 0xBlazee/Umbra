38d36139794b33736e0a3908843b9d0aed352661
sorrow <cblu25271@gmail.com>
2026-08-12 21:12:41 +0200

    agent.cpp

--- agent.cpp
+++ agent.cpp
@@ -1,11 +1,17 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <windows.h>
+
 #include <winsock2.h>
-#include <ws2tcpip.h>
+#include <windows.h>
+
+#include <cstdint>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
 
 #pragma comment(lib, "Ws2_32.lib")
 
